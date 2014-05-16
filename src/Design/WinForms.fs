8dbfe95a7f704cf1d98bf2fc64f814d13c127b70
Heather <heather@live.ru>
2014-05-16 09:14:12 +0400

    improve syntax highlighting

--- src/Design/WinForms.fs
+++ src/Design/WinForms.fs
@@ -20,24 +20,41 @@ type RichTextBoxWithSqlHighlighting() =
     override x.OnTextChanged(e : System.EventArgs) =
         base.OnTextChanged(e); x.ColorTheKeyWords()
     member x.ColorTheKeyWords() =
+        let color(m : Match, color : Color) =
+            x.SelectionStart    <- m.Index
+            x.SelectionLength   <- m.Length
+            x.SelectionColor    <- color
         let hl s c =
-            let color(m : Match, color : Color) =
-                x.SelectionStart    <- m.Index
-                x.SelectionLength   <- m.Length
-                x.SelectionColor    <- color
             Regex.Matches(x.Text, "\\b" + s + "\\b", RegexOptions.IgnoreCase) |> fun mx ->
                 for m in mx do if (m.Success) then color(m,c)
         let SelectionAt = x.SelectionStart
         Lock.LockWindowUpdate(x.Handle.ToInt32())
-        hl "(\*)|(!)|(~)|(,)|(@)|(let)" Color.Blue
-        hl "(\()|(\))|(\[)|(\])" Color.DarkGray
-        hl "(and)|(or)|(not)" Color.DarkGreen
-        hl "(avg)|(abs)|(max)|(min)" Color.DarkRed
-        hl "(select)|(where)|(from)|(top)|(order)|(group)|(by)|(as)|(null)|(insert)|(exec)|(into)" Color.Blue
-        hl "(join)|(left)|(inner)|(outer)|(right)|(on)" Color.Red
-        hl "(case)|(when)|(then)|(else)|(end)|(if)|(begin)" Color.Teal
-        hl "(cast)|(nvarchar)|(bit)|(datetime)|(int)|(table)" Color.BlueViolet
-        hl "(datepart)" Color.DarkOrange
+        hl "(\*)|(!)|(~)|(,)|(@)" Color.Blue
+        hl "(\()|(\))" Color.DarkGray
+
+        let hlW (w : string) (s : string) c =
+            let mutable docolor = false
+            for mtch in s.Split('(','|',')') do
+                if not docolor && mtch <> "" && w.ToLower() = mtch then 
+                    docolor <- true
+                    Regex.Matches(x.Text, "\\b" + s + "\\b", RegexOptions.IgnoreCase)
+                    |> fun mx -> for m in mx do if (m.Success) then color(m,c)
+        for word in x.Text.Split( ' ', '*', '!', '~', ',', '@'
+                                , '(', ')', '[', ']'
+                                , '\n' ,'\r' ,'\t') do
+            hlW word "(let)" Color.Blue
+            hlW word "(and)|(or)|(not)" Color.DarkGreen
+            hlW word "(avg)|(abs)|(max)|(min)" Color.DarkRed
+            hlW word "(select)|(where)|(from)|(top)|(order)|(group)|(by)|(as)|(null)|(insert)|(exec)|(into)" Color.Blue
+            hlW word "(desc)|(asc)" Color.Brown
+            hlW word "(join)|(left)|(inner)|(outer)|(right)|(on)" Color.Red
+            hlW word "(case)|(when)|(then)|(else)|(end)|(if)|(begin)" Color.Teal
+            hlW word "(cast)|(nvarchar)|(bit)|(datetime)|(int)|(table)" Color.BlueViolet
+            hlW word "(datepart)" Color.DarkOrange
+
+        Regex.Matches(x.Text, "\[(.*?)\]", RegexOptions.IgnoreCase) |> fun mx ->
+            for m in mx do if (m.Success) then color(m,Color.DarkBlue)
+            
         x.SelectionStart    <- SelectionAt
         x.SelectionLength   <- 0
         x.SelectionColor    <- Color.Black
